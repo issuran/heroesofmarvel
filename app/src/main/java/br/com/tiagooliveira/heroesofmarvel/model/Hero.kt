f394443c356646786a97a1a1e58e397580dfb86c
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2018-04-14 14:09:28 -0300

    Changed model to Kotlin

---
+++ app/src/main/java/br/com/tiagooliveira/heroesofmarvel/model/Hero.kt
@@ -0,0 +1,5 @@
+package br.com.tiagooliveira.heroesofmarvel.model
+
+data class Hero constructor(val name: String = "",
+                            val description: String = "",
+                            val image: String = "http://freepngimg.com/download/megaman/20003-5-megaman.png")
---
+++ app/src/main/java/br/com/tiagooliveira/heroesofmarvel/service/HeroesApiService.kt
@@ -0,0 +1,2 @@
+package br.com.tiagooliveira.heroesofmarvel.service
+
